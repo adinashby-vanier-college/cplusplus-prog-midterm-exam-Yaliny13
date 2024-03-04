9af5a16f1979f467ad7b5c1749b9855537650909
Yaliny13 <158082272+Yaliny13@users.noreply.github.com>
2024-03-04 12:00:29 -0500

    Q1/Q2 Completed

--- labFile.hpp
+++ labFile.hpp
@@ -15,7 +15,16 @@ double fahrenheitToKelvin(double fahrenheit) {
 }
 
 bool isPrime(int n) {
-    return false;
+    if (n <= 1)
+        return false;
+   
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
 }
 
 int sumOfPrimes(int limit) {
